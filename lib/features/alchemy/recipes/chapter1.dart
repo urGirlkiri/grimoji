c54a5b5d58c4cd8cbd8394d46563b5ed426255b2
Kiri <cultivatingghoul@gmail.com>
2026-05-30 22:43:03 +0200

    poulrofietryLeg typo :)

--- lib/features/alchemy/recipes/chapter1.dart
+++ lib/features/alchemy/recipes/chapter1.dart
@@ -21,7 +21,7 @@ const List<Recipe> chapter1Recipes = [
   ),
 
   Recipe(
-    ingredient: Emojis.poulrofietryLeg,
+    ingredient: Emojis.poultryLeg,
     requiredAmount: 3,
     yields: Emojis.bone,
   ),
