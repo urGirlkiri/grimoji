14f3e03f136e39fb8cc434c5e6151fe27fd0f65b
Kiri <cultivatingghoul@gmail.com>
2026-06-08 10:15:21 +0200

    fixed game resetting on resize

--- lib/features/cauldron/game/screen.dart
+++ lib/features/cauldron/game/screen.dart
@@ -4,18 +4,35 @@ import 'package:grimoji/features/cauldron/game/index.dart';
 import 'package:grimoji/features/level/hint_screen/loading.dart';
 import 'package:grimoji/utils/context_data.dart';
 
-class CauldronPlayScreen extends StatelessWidget {
+class CauldronPlayScreen extends StatefulWidget {
   const CauldronPlayScreen({super.key});
+
+  @override
+  State<CauldronPlayScreen> createState() => _CauldronPlayScreenState();
+}
+
+class _CauldronPlayScreenState extends State<CauldronPlayScreen> {
+  late final CauldronGame _game;
+  bool _isGameInitialized = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_isGameInitialized) {
+      _game = CauldronGame(
+        colorScheme: context.theme.colorScheme,
+        globalScale: context.globalScale,
+        context: context,
+      );
+      _isGameInitialized = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GameWidget(
-        game: CauldronGame(
-          colorScheme: context.theme.colorScheme,
-          globalScale: context.globalScale,
-          context: context
-        ),
+        game: _game,
         loadingBuilder: (context) => const Loading(),
       ),
     );
