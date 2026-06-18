f38a5b1ecd66b3d6a0aefb0ecf6543f68028e6e9
Kiri <cultivatingghoul@gmail.com>
2026-06-18 13:14:42 +0200

    added swallow behaviour

---
+++ lib/features/alchemy/behaviors/swallow.dart
@@ -0,0 +1,28 @@
+import 'package:grimoji/config/emojis/index.dart';
+import 'package:grimoji/features/alchemy/behaviors/behavior.dart';
+import 'package:grimoji/features/alchemy/models/action_type.dart';
+import 'package:grimoji/features/alchemy/models/behavior_action.dart';
+
+class SwallowBehavior extends EmojiBehavior {
+
+  static final triggerEmoji = Emojis.hole;
+
+  @override
+  List<BehaviorAction> onSwipedWith(int x, int y, GameEmoji targetEmoji) {
+    if (targetEmoji == triggerEmoji) {
+      return [
+        const BehaviorAction(
+          type: ActionType.consumeAllOfType,
+          emoji: null, x: null, y: null, 
+        )
+      ];
+    }
+
+    return [
+      BehaviorAction(
+        type: ActionType.consumeAllOfType,
+        emoji: targetEmoji,
+      )
+    ];
+  }
+}
