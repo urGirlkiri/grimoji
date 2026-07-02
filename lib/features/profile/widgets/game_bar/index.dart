fda8e754718417d6715753d235aa1e28f4ed44d8
Kiri <cultivatingghoul@gmail.com>
2026-07-02 10:22:53 +0200

    made the gamebar shell into a common widget

--- lib/features/profile/widgets/game_bar/index.dart
+++ lib/features/profile/widgets/game_bar/index.dart
@@ -11,6 +11,7 @@ import 'package:grimoji/utils/context_data.dart';
 import 'package:grimoji/widgets/animations/dialog.dart';
 import 'package:grimoji/widgets/custom/animated_button.dart';
 import 'package:grimoji/widgets/custom/app_icon.dart';
+import 'package:grimoji/widgets/custom/plaque.dart';
 import 'package:provider/provider.dart';
 
 class GameBar extends StatelessWidget {
@@ -32,96 +33,50 @@ class GameBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final scale = context.globalScale;
-    final topPadding = MediaQuery.paddingOf(context).top;
 
-    return SizedBox(
-      height: (105.0 * scale) + topPadding,
-      child: Container(
-        color: backgroundColor,
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              height: (90.0 * scale) + topPadding,
-              decoration: BoxDecoration(
-                color: context.palette.midnight,
-                borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.circular(24.0),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: context.palette.voidBlack,
-                    offset: Offset(0, 6 * scale),
-                    blurRadius: 0,
-                  ),
-                  BoxShadow(
-                    color: context.palette.voidBlack.withValues(alpha: 0.3),
-                    offset: Offset(0, 10 * scale),
-                    blurRadius: 10,
-                    spreadRadius: 1,
-                  ),
-                ],
-                image: DecorationImage(
-                  image: const AssetImage('assets/images/vertical_lines.png'),
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(
-                    context.palette.midnight.withValues(alpha: 0.069),
-                    BlendMode.dstATop,
-                  ),
-                ),
-              ),
+    return GamePlaque(
+      backgroundColor: backgroundColor,
+      pchild: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AppIcon(
+            fileName: 'mail_inbox',
+            size: 30 * scale,
+            onTap: () => onNotifTap(context),
+          ),
+          const SizedBox(width: 8),
+          Selector<ProfileController, String>(
+            selector: (_, profile) =>
+                profile.cauldrons >= 5 ? 'Full' : profile.cauldrons.toString(),
+            builder: (context, cauldrons, _) => ResourcePill(
+              iconPath: 'assets/images/cauldron.png',
+              value: cauldrons,
+              onTap: () => onCauldronTap(context),
             ),
-            Positioned(
-              top: (20 * scale) + topPadding,
-              left: 12 * scale,
-              right: 12 * scale,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  AppIcon(
-                    fileName: 'mail_inbox',
-                    size: 30 * scale,
-                    onTap: () => onNotifTap(context),
-                  ),
-                  const SizedBox(width: 8),
-                  Selector<ProfileController, String>(
-                    selector: (_, profile) => profile.cauldrons >= 5
-                        ? 'Full'
-                        : profile.cauldrons.toString(),
-                    builder: (context, cauldrons, _) => ResourcePill(
-                      iconPath: 'assets/images/cauldron.png',
-                      value: cauldrons,
-                      onTap: () => onCauldronTap(context),
-                    ),
-                  ),
-                  Selector<ProfileController, String>(
-                    selector: (_, profile) => profile.avatar,
-                    builder: (context, avatar, _) => AnimatedButton(
-                      onTap: () => onProfileTap(context),
-                      child: ProfileAvatar(avatar: avatar),
-                    ),
-                  ),
-                  Selector<ProfileController, int>(
-                    selector: (_, profile) => profile.dices,
-                    builder: (context, dices, _) => ResourcePill(
-                      iconPath: 'assets/images/dice.png',
-                      value: dices.toString(),
-                      onTap: () => GoRouter.of(context).goNamed(Routes.market),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  AppIcon(
-                    fileName: 'settings',
-                    size: 30 * scale,
-                    onTap: () =>
-                        GoRouter.of(context).pushNamed(Routes.settings),
-                  ),
-                ],
-              ),
+          ),
+          Selector<ProfileController, String>(
+            selector: (_, profile) => profile.avatar,
+            builder: (context, avatar, _) => AnimatedButton(
+              onTap: () => onProfileTap(context),
+              child: ProfileAvatar(avatar: avatar),
             ),
-          ],
-        ),
+          ),
+          Selector<ProfileController, int>(
+            selector: (_, profile) => profile.dices,
+            builder: (context, dices, _) => ResourcePill(
+              iconPath: 'assets/images/dice.png',
+              value: dices.toString(),
+              onTap: () => GoRouter.of(context).goNamed(Routes.market),
+            ),
+          ),
+          const SizedBox(width: 8),
+          AppIcon(
+            fileName: 'settings',
+            size: 30 * scale,
+            onTap: () => GoRouter.of(context).pushNamed(Routes.settings),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/custom/plaque.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:grimoji/utils/context_data.dart';
+
+class GamePlaque extends StatelessWidget {
+  final Widget? pchild;
+  final Widget? child;
+  final Color backgroundColor;
+  final double? height;
+
+  const GamePlaque({
+    super.key,
+    this.pchild,
+    this.child,
+    this.backgroundColor = Colors.transparent,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final scale = context.globalScale;
+    final topPadding = MediaQuery.paddingOf(context).top;
+    final plaqueHeight = height ?? (90.0 * scale) + topPadding;
+
+    return SizedBox(
+      height: plaqueHeight + 15 * scale,
+      child: Container(
+        color: backgroundColor,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: plaqueHeight,
+              decoration: BoxDecoration(
+                color: context.palette.midnight,
+                borderRadius: const BorderRadius.vertical(
+                  bottom: Radius.circular(24.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: context.palette.voidBlack,
+                    offset: Offset(0, 6 * scale),
+                    blurRadius: 0,
+                  ),
+                  BoxShadow(
+                    color: context.palette.voidBlack.withValues(alpha: 0.3),
+                    offset: Offset(0, 10 * scale),
+                    blurRadius: 10,
+                    spreadRadius: 1,
+                  ),
+                ],
+                image: DecorationImage(
+                  image: const AssetImage('assets/images/vertical_lines.png'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    context.palette.midnight.withValues(alpha: 0.069),
+                    BlendMode.dstATop,
+                  ),
+                ),
+              ),
+            ),
+            if (pchild != null)
+              Positioned(
+                top: (20 * scale) + topPadding,
+                left: 12 * scale,
+                right: 12 * scale,
+                child: pchild!,
+              ),
+            ?child,
+          ],
+        ),
+      ),
+    );
+  }
+}
