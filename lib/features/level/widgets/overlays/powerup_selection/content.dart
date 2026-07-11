33eab138050ca311fe761237ecc0a04271b7d16a
Kiri <cultivatingghoul@gmail.com>
2026-07-11 19:50:44 +0200

    made theslected powerup content cove the entire header

--- lib/features/level/widgets/overlays/powerup_selection/content.dart
+++ lib/features/level/widgets/overlays/powerup_selection/content.dart
@@ -10,22 +10,28 @@ class Content extends StatelessWidget {
 
     return Container(
       color: Colors.transparent,
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          decoration: BoxDecoration(
-            color: palette.trueWhite,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: palette.voidBlack.withValues(alpha: 0.3),
-                blurRadius: 10,
-                offset: const Offset(0, 4),
-              ),
-            ],
-          ),
+      child: Container(
+        padding: const EdgeInsets.only(
+          left: 95,
+          right: 95,
+          top: 80,
+          bottom: 60,
+        ),
+        decoration: BoxDecoration(
+          color: palette.dusk,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: palette.voidBlack.withValues(alpha: 0.3),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Select a tile',
@@ -36,10 +42,12 @@ class Content extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 8),
-               Text(
+              Text(
                 'Tap any tile to use the powerup',
-                style: TextStyle(fontSize: 14, color: palette.voidBlack,)
+                style: TextStyle(fontSize: 14, color: palette.voidBlack),
               ),
+              const SizedBox(height: 8),
+              // EmojiWidget(assetPath: )
             ],
           ),
         ),
