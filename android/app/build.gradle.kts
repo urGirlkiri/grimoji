c208a0907373b616147f8ee39d19d111bf8aa08c
Kiri <cultivatingghoul@gmail.com>
2026-07-16 19:48:17 +0200

    added local notifications native setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,6 +21,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -54,6 +55,10 @@ android {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
