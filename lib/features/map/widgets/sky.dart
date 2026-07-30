8dc9d6f8c47fb16ce580e8f372fe0ef810aafd67
Kiri <cultivatingghoul@gmail.com>
2026-07-30 20:35:39 +0200

    adjusted sky for best distant effect

--- lib/features/map/widgets/sky.dart
+++ lib/features/map/widgets/sky.dart
@@ -3,20 +3,23 @@ import 'package:grimoji/config/emojis/index.dart';
 import 'package:grimoji/widgets/custom/emoji_widget.dart';
 
 class Sky extends StatelessWidget {
-  const Sky({
-    super.key,
-  });
+  const Sky({super.key});
 
+  static const size = 65.0;
+  static const opacity = 0.0345;
   @override
   Widget build(BuildContext context) {
     return Opacity(
-      opacity: 0.08,
+      opacity: opacity,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          EmojiWidget.lottie(path: Emojis.cloud.lottie, size: 80,),
-          EmojiWidget.lottie(path: Emojis.cloudWithLightning.lottie, size: 80,),
-          EmojiWidget.lottie(path: Emojis.rainCloud.lottie, size: 80,),
+          EmojiWidget.lottie(path: Emojis.rainCloud.lottie, size: size),
+          EmojiWidget.lottie(
+            path: Emojis.cloudWithLightning.lottie,
+            size: size,
+          ),
+          EmojiWidget.lottie(path: Emojis.rainCloud.lottie, size: size),
         ],
       ),
     );
