11a99dd8571a7cdd3146986cdd2d17b7dc242721
Kiri <cultivatingghoul@gmail.com>
2026-08-01 08:27:54 +0200

    grouped theme n palette together

--- lib/app/theme/index.dart
+++ lib/app/theme/index.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:grimoji/app/palette.dart';
+import 'package:grimoji/app/theme/palette.dart';
 
 class AppTheme {
   static ThemeData buildTheme(Palette palette, double scale) {
