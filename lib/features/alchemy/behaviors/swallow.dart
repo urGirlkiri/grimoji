2a25f8126530f5da5f5516189026651de2e863ef
Kiri <cultivatingghoul@gmail.com>
2026-08-10 13:09:06 +0200

    stopped black hole from swallowing when its already destroyed

--- lib/features/alchemy/behaviors/swallow.dart
+++ lib/features/alchemy/behaviors/swallow.dart
@@ -2,7 +2,6 @@ import 'package:grimoji/config/emojis/index.dart';
 import 'package:grimoji/features/alchemy/behaviors/models/behavior.dart';
 import 'package:grimoji/features/alchemy/behaviors/models/action_type.dart';
 import 'package:grimoji/features/alchemy/behaviors/models/behavior_action.dart';
-import 'package:grimoji/features/alchemy/reactions/models/reaction.dart';
 
 class SwallowBehavior extends EmojiBehavior {
   static final emoji = Emojis.hole;
@@ -30,15 +29,5 @@ class SwallowBehavior extends EmojiBehavior {
     return [
       const BehaviorAction(type: ActionType.consumeRandomType, emoji: null),
     ];
-  }
-
-  @override
-  List<BehaviorAction> onBlastNearby(int x, int y, ReactionType reactionType) {
-    if (reactionType == ReactionType.explosive) {
-      return [
-        const BehaviorAction(type: ActionType.consumeRandomType, emoji: null),
-      ];
-    }
-    return [];
   }
 }
