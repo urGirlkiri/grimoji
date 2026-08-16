8fd679786275c27af8db65cfa04dc5c723a0ef9a
Kiri <cultivatingghoul@gmail.com>
2026-08-16 13:19:53 +0200

    reset emoji behaviours in test grid helper

--- test/helpers/test_grid.dart
+++ test/helpers/test_grid.dart
@@ -1,5 +1,6 @@
 import 'package:grimoji/config/emojis/index.dart';
 import 'package:grimoji/features/match/board/manager.dart';
+import 'package:grimoji/features/match/models/tile.dart';
 
 class TestGrid {
   final BoardManager board;
@@ -9,33 +10,41 @@ class TestGrid {
   void fill(GameEmoji emoji) {
     for (int r = 0; r < BoardManager.rows; r++) {
       for (int c = 0; c < BoardManager.cols; c++) {
-        board.gridTiles[r][c].emoji = emoji;
+        _setEmoji(board.gridTiles[r][c], emoji);
       }
     }
   }
 
   void place(int r, int c, GameEmoji emoji) {
-    board.gridTiles[r][c].emoji = emoji;
+    _setEmoji(board.gridTiles[r][c], emoji);
   }
 
   void fillRow(int r, GameEmoji emoji) {
     for (int c = 0; c < BoardManager.cols; c++) {
-      board.gridTiles[r][c].emoji = emoji;
+      _setEmoji(board.gridTiles[r][c], emoji);
     }
   }
 
   void fillCol(int c, GameEmoji emoji) {
     for (int r = 0; r < BoardManager.rows; r++) {
-      board.gridTiles[r][c].emoji = emoji;
+      _setEmoji(board.gridTiles[r][c], emoji);
     }
   }
 
   void fillPattern(List<GameEmoji> pattern) {
     for (int r = 0; r < BoardManager.rows; r++) {
       for (int c = 0; c < BoardManager.cols; c++) {
-        board.gridTiles[r][c].emoji =
-            pattern[(r * BoardManager.cols + c) % pattern.length];
+        _setEmoji(
+          board.gridTiles[r][c],
+          pattern[(r * BoardManager.cols + c) % pattern.length],
+        );
       }
     }
+  }
+
+  void _setEmoji(Tile tile, GameEmoji emoji) {
+    tile.emoji = emoji;
+    tile.reset();
+    tile.clearBehavior();
   }
 }
