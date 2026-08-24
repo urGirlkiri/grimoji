5397d3f6025f18c3e7f8d1f4e4a9c8b09e99533f
Kiri <cultivatingghoul@gmail.com>
2026-08-24 08:06:19 +0200

    extracted powerup bump from footer

--- lib/features/level/widgets/footer/index.dart
+++ lib/features/level/widgets/footer/index.dart
@@ -4,6 +4,7 @@ import 'package:grimoji/config/powerups.dart';
 import 'package:grimoji/features/level/state.dart';
 import 'package:grimoji/features/level/widgets/dialogs/purchase_dialog/index.dart';
 import 'package:grimoji/features/level/widgets/footer/powerup.dart';
+import 'package:grimoji/features/level/widgets/footer/powerup_bump.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/index.dart';
 import 'package:grimoji/features/profile/controller.dart';
 import 'package:grimoji/utils/context_data.dart';
@@ -133,7 +134,7 @@ class _FooterState extends State<Footer> {
                 () => GlobalKey(),
               );
               return [
-                _PowerupBump(
+                PowerupBump(
                   token: _levelState.bumpPowerupToken(powerup.id),
                   child: PowerupBtn(
                     key: btnKey,
@@ -148,70 +149,6 @@ class _FooterState extends State<Footer> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class _PowerupBump extends StatefulWidget {
-  final ValueNotifier<int> token;
-  final Widget child;
-
-  const _PowerupBump({required this.token, required this.child});
-
-  @override
-  State<_PowerupBump> createState() => _PowerupBumpState();
-}
-
-class _PowerupBumpState extends State<_PowerupBump>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _scale;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 320),
-    );
-    _scale = TweenSequence<double>([
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 1.0,
-          end: 1.35,
-        ).chain(CurveTween(curve: Curves.easeOut)),
-        weight: 40,
-      ),
-      TweenSequenceItem(
-        tween: Tween(
-          begin: 1.35,
-          end: 1.0,
-        ).chain(CurveTween(curve: Curves.easeIn)),
-        weight: 60,
-      ),
-    ]).animate(_controller);
-    widget.token.addListener(_onBump);
-  }
-
-  void _onBump() {
-    if (!mounted) return;
-    _controller.forward(from: 0);
-  }
-
-  @override
-  void dispose() {
-    widget.token.removeListener(_onBump);
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _scale,
-      builder: (context, child) =>
-          Transform.scale(scale: _scale.value, child: child),
-      child: widget.child,
     );
   }
 }
---
+++ lib/features/level/widgets/footer/powerup_bump.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class PowerupBump extends StatefulWidget {
+  final ValueNotifier<int> token;
+  final Widget child;
+
+  const PowerupBump({super.key, required this.token, required this.child});
+
+  @override
+  State<PowerupBump> createState() => PowerupBumpState();
+}
+
+
+
+class PowerupBumpState extends State<PowerupBump>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _scale;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 320),
+    );
+    _scale = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.0,
+          end: 1.35,
+        ).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 40,
+      ),
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.35,
+          end: 1.0,
+        ).chain(CurveTween(curve: Curves.easeIn)),
+        weight: 60,
+      ),
+    ]).animate(_controller);
+    widget.token.addListener(_onBump);
+  }
+
+  void _onBump() {
+    if (!mounted) return;
+    _controller.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    widget.token.removeListener(_onBump);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _scale,
+      builder: (context, child) =>
+          Transform.scale(scale: _scale.value, child: child),
+      child: widget.child,
+    );
+  }
+}
