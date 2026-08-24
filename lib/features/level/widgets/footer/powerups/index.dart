74d57015ff14b99453c75fdf23df05db1648f159
Kiri <cultivatingghoul@gmail.com>
2026-08-24 08:30:06 +0200

    added test tubepowerup  handler

--- lib/features/level/widgets/footer/powerups/index.dart
+++ lib/features/level/widgets/footer/powerups/index.dart
@@ -2,12 +2,14 @@ import 'package:grimoji/features/level/models/powerup_handler.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/blood.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/hourglass.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/punch.dart';
+import 'package:grimoji/features/level/widgets/footer/powerups/testtube.dart';
 
 class PowerupHandlerRegistry {
   static final Map<String, PowerupHandler> _handlers = {
     'hourglass': HourglassHandler(),
     'boxing_glove': PunchHandler(),
     'blood': BloodHandler(),
+    'test_tube': TestTubeHandler()
   };
 
   static PowerupHandler? get(String id) => _handlers[id];
---
+++ lib/features/level/widgets/footer/powerups/testtube.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:grimoji/config/powerups.dart';
+import 'package:grimoji/features/level/models/powerup_handler.dart';
+import 'package:grimoji/features/level/state.dart';
+import 'package:logging/logging.dart';
+
+class TestTubeHandler implements PowerupHandler {
+  static final _log = Logger('TestTubeHandler');
+
+  @override
+  Future<void> execute(
+    BuildContext context,
+    Powerup powerup,
+    LevelState levelState,
+  ) async {
+    try {
+      final selected = await levelState.awaitPowerupTile(powerup);
+
+      if (levelState.gameState.isGameOver || levelState.gameState.isPaused) {
+        return;
+      }
+
+      _log.info('Selected tile: ${selected.row}, ${selected.col}');
+
+      // levelState.onPowerupImpact = () {
+      //   levelState.coordinator.testTubeImpact(selected);
+      // };
+
+      final animation = levelState.startPowerupAnimation();
+      // levelState.triggerTestTube(selected);
+
+      await animation;
+
+      await levelState.coordinator.bloodTile(selected);
+    } catch (e) {
+      _log.info('Cancelled: $e');
+      levelState.updatePowerupHoverTarget(null);
+    }
+  }
+}
