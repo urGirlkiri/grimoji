88caa95a78edef27cafb72e1c7cdf9c803f3376e
Kiri <cultivatingghoul@gmail.com>
2026-08-30 11:04:47 +0200

    added ufo handler

--- lib/features/level/widgets/footer/powerups/index.dart
+++ lib/features/level/widgets/footer/powerups/index.dart
@@ -3,13 +3,15 @@ import 'package:grimoji/features/level/widgets/footer/powerups/blood.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/hourglass.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/punch.dart';
 import 'package:grimoji/features/level/widgets/footer/powerups/testtube/index.dart';
+import 'package:grimoji/features/level/widgets/footer/powerups/ufo/index.dart';
 
 class PowerupHandlerRegistry {
   static final Map<String, PowerupHandler> _handlers = {
     'hourglass': HourglassHandler(),
     'boxing_glove': PunchHandler(),
     'blood': BloodHandler(),
-    'test_tube': TestTubeHandler()
+    'test_tube': TestTubeHandler(),
+    'ufo': UFOHandler(),
   };
 
   static PowerupHandler? get(String id) => _handlers[id];
---
+++ lib/features/level/widgets/footer/powerups/ufo/index.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:grimoji/config/powerups.dart';
+import 'package:grimoji/features/level/models/powerup_handler.dart';
+import 'package:grimoji/features/level/state.dart';
+import 'package:logging/logging.dart';
+
+class UFOHandler implements PowerupHandler {
+  static final _log = Logger('UFOHandler');
+
+  @override
+  Future<void> execute(
+    BuildContext context,
+    Powerup powerup,
+    LevelState levelState,
+  ) async {
+    try {
+
+      if (levelState.gameState.isGameOver || levelState.gameState.isPaused) {
+        return;
+      }
+
+      // final grid = levelState.coordinator.engine.grid;
+      // final analysis = await UFOAnalyzer.analyzeTarget(
+      //   grid: grid,
+      // );
+
+      levelState.onPowerupImpact = () async {
+        // await levelState.coordinator.testTubeImpact(
+        //   analysis.bestEmoji,
+        // );
+      };
+
+      // final animation = levelState.startPowerupAnimation();
+      // levelState.triggerUFOs();
+
+      // await animation;
+    } catch (e) {
+      _log.info('Cancelled: $e');
+      levelState.updatePowerupHoverTarget(null);
+    }
+  }
+}
